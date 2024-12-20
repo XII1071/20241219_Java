b06f46c8e8ba98c4d0ba61c91fe9b31106bcbfe3
XII1071 <garmen1071@gmail.com>
2024-12-20 10:42:32 +0900

    1220

--- src/common/Util.java
+++ src/common/Util.java
@@ -2,7 +2,7 @@ package common;
 
 public class Util {
   public static void typeof(Object obj) { //typeof(); 변수를 사용할때 타입하고 변수가 들어간다.
-    System.out.println(obj.getClass().getSimpleName()); //void는 타입이 비어있다!
+    System.out.println(obj.getClass().getSimpleName()); //void는 타입이 비어있다! return 값이 없다!
   }
 }
 
