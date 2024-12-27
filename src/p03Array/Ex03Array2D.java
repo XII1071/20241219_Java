fdd8037f9f51ca429c8f5da7f7d95561f3561518
XII1071 <garmen1071@gmail.com>
2024-12-27 14:42:29 +0900

    1227

--- src/p03Array/Ex03Array2D.java
+++ src/p03Array/Ex03Array2D.java
@@ -8,11 +8,15 @@ public class Ex03Array2D {
 
     int[][] arr2 = new int[4][4];
     for (int i = 0; i < arr.length; i++) {
+      int tot = 0;
       for (int j = 0; j < arr[i].length; j++) {
         arr2[i][j] = arr[i][j];
         arr2[i][arr.length] += arr[i][j];
         arr2[arr.length][j] += arr[i][j];
+//        arr2[arr.length][arr[i].length] += arr[i][j];
+        tot += arr[i][j];
       }
+      arr2[arr2.length - 1][arr2[arr2.length - 1].length - 1] += tot;
     }
     print2d(arr2);
   }
