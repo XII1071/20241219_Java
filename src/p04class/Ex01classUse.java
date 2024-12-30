d0d2c677d426b2e8ca8f97e235dde49188f148a3
XII1071 <garmen1071@gmail.com>
2024-12-30 14:42:47 +0900

    Merge remote-tracking branch 'origin/master'

--- src/p04class/Ex01classUse.java
+++ src/p04class/Ex01classUse.java
@@ -1,3 +1,4 @@
+
 package p04class;
 
 public class Ex01classUse {
@@ -8,9 +9,14 @@ public class Ex01classUse {
 
     // has-A관계 :: 자동차는 엔진이다(X) / 엔진은 자동차이다(X)
     Car car = new Car();
-    System.out.println(car);
+    car.model = "Hyundae";
+    System.out.println(car); //engine은 초기화 안됨. null
     car.engine = new Engine();
+    car.engine.model = "알파엔진";
+
     System.out.println(car.engine.gitong);
+    System.out.println(car.model);
+    System.out.println(car.engine.model);
 
     // is-A 관계 :: 소나무는 나무다(O) / 나무는 소나무다(X)
     Tree tree = new Tree();
@@ -32,7 +38,11 @@ class Car {
 
 class Engine {
   int gitong;
+  String model;
 }
+
+//자바에서는 다중 상속이 안됨(오직 단일 상속)
+//class SportsCar extends Car, Engine{}
 
 class Tree {
 }
--- src/p04class/Ex04ExtendsCasting.java
+++ src/p04class/Ex04ExtendsCasting.java
@@ -2,17 +2,46 @@ package p04class;
 
 public class Ex04ExtendsCasting {
   public static void main(String[] args) {
-    // 상속 관계에서 형변환이 발생하면서, 중복되는 경우
+    Father f1 = new Father("아빠", 40);
+    Son s1 = new Son("아들", 10);
+    System.out.println(f1);
+    System.out.println(s1);
+    System.out.println(s1.name);
+    System.out.println(s1.age);
+    // 상속 관계에서 형변환이 발생하면서, 속성이 중복되는 경우
     // 1) 자식 -> 부모
 
   }
 }
 
 class Father {
+  int age;
+  String name;
+
+  // 생성자를 추가하면 더이상 JVM이 기본 생성자는 안만듬
+  public Father(String name, int age) {
+
+    this.name = name;
+    this.age = age;
+
+  }
+
+  @Override
+  public String toString() {
+    return "Father:" + age + "/" + name;
+  }
 }
 
-class Mother {
-}
+class Son extends Father {
+  public Son(String name, int age) {
+    super(name, age);
+  }
 
-class Son {
+  int age;
+  String name;
+
+  @Override
+  public String toString() {
+    return String.format("Son: %d / %s", super.age, super.name);
+  }
 }
