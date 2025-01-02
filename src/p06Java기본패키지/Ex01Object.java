a390aa60a9711fa0280681812778c3c76f6454d7
XII1071 <garmen1071@gmail.com>
2025-01-02 16:05:44 +0900

    0102

--- src/p06Java기본패키지/Ex01Object.java
+++ src/p06Java기본패키지/Ex01Object.java
@@ -1,11 +1,13 @@
 package p06Java기본패키지;
 
+import java.util.Objects;
+
 public class Ex01Object {
   public static void main(String[] args) {
     Object obj = new Object();
     // 1) toString()
-    System.out.println("obj" + obj); //toString
-    System.out.println("obj" + obj.toString()); //toString
+    System.out.println("obj: " + obj); //toString
+    System.out.println("obj: " + obj.toString()); //toString
     System.out.println(System.identityHashCode(obj));
     MyObj myObj1 = new MyObj();
     System.out.println(myObj1);
@@ -14,13 +16,21 @@ public class Ex01Object {
     System.out.println(myObj2);
     System.out.println(System.identityHashCode(myObj2));
     System.out.println("myObj1 == myObj2 : " + (myObj1 == myObj2));
-    System.out.println("myObj1 equals(myObj2) : " + (myObj1.equals(myObj2)));
+    System.out.println("myObj1.equals(myObj2) : " + (myObj1.equals(myObj2)));
+    MyObj myObj3 = null;
+    try {
+      myObj3 = (MyObj) myObj1.clone();
+    } catch (CloneNotSupportedException e) {
+      throw new RuntimeException(e);
+    }
+    System.out.println("myObj1 == myObj3 : " + (myObj1 == myObj3));
+    System.out.println("myObj1.equals(myObj3) : " + (myObj1.equals(myObj3)));
   }
 }
 
-class MyObj {
-  // getClass(), hashCode(), equals(Object obj), clone(), toString(),
-  // notify(), notifyAll(), wait(), finalize()
+class MyObj implements Cloneable {
+  //getClass(),hashCode(),equals(Object obj),clone(),toString(),
+  //notify(),notifyAll(),wait(),finalize()
   int val = 100;
 
   @Override
@@ -36,5 +46,21 @@ class MyObj {
       myObj = (MyObj) obj;
     }
     return myObj.val == this.val;
+  }
+
+  // clone()를 사용하기 위해 Cloneable을 implement하고 hashCode(), clone()재정의
+  @Override
+  public int hashCode() {
+    return Objects.hash(val);
+  }
+
+  @Override
+  protected Object clone() throws CloneNotSupportedException {
+    Object obj = null;
+    try {
+      obj = super.clone(); //MyObject => Object (자->부)
+    } catch (CloneNotSupportedException e) {
+    }
+    return obj;
   }
 }
