ffdebd781302b04b616d88e4444b7af927f2566a
XII1071 <garmen1071@gmail.com>
2025-01-03 16:58:29 +0900

    0103

--- src/p07Collection/Ex01ArrayList.java
+++ src/p07Collection/Ex01ArrayList.java
@@ -6,7 +6,9 @@ import java.util.List;
 
 public class Ex01ArrayList {
   public static void main(String[] args) {
-    List listInt = new ArrayList<>();
+    List listInt = new ArrayList(); // 기본 생성자로 ArrayList 생성
+    List listCopy = new ArrayList(listInt); //Collection으로 ArrayList 생성
+    List listCapacity = new ArrayList(10); // 크기로 ArrayList 생성
     listInt.add(0);
     listInt.add(1);
     listInt.add(2);
@@ -22,35 +24,37 @@ public class Ex01ArrayList {
 
 
     List list = new ArrayList<>();
-    Member m1 = new Member("1", "강지원", "lgh");
+    Member m1 = new Member("lgh", "이규훈", "1");
     list.add(m1);
-    list.add(new Member("1", "G1", "mgh"));
-    list.add(new Member("1", "지원", "rgh"));
+    list.add(new Member("mgh", "마규훈", "1"));
+    list.add(new Member("rgh", "라규훈", "1"));
     System.out.println(list);
+
     for (int i = 0; i < list.size(); i++) {
       System.out.println(list.get(i));
     }
+
     for (Object o : list) {
       System.out.println((Member) o);
     }
+
     Iterator it = list.iterator();
     while (it.hasNext()) {
       System.out.println(it.next());
     }
+
+
   }
 }
 
 class Member {
-
-
   private String id, name, pass;
 
-  public Member(String pass, String name, String id) {
+  public Member(String id, String name, String pass) {
     this.id = id;
     this.name = name;
     this.pass = pass;
   }
-
 
   public String getId() {
     return id;
@@ -78,6 +82,6 @@ class Member {
 
   @Override
   public String toString() {
-    return String.format("Member{id=%s, name=%s,pass=%s}", id, name, pass);
+    return String.format("id=%s,name=%s,pass=%s", id, name, pass);
   }
 }
