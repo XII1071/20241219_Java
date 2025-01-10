667587a46d33a3c2f52809814a75b6b2a7201e85
XII1071 <garmen1071@gmail.com>
2025-01-10 16:56:04 +0900

    0110

--- src/p13database/ui/FrmLogin.java
+++ src/p13database/ui/FrmLogin.java
@@ -1,7 +1,12 @@
 package p13database.ui;
 
+import p13database.dao.DAOMember;
+import p13database.vo.MemberVO;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FrmLogin extends FrmBasic {
   private JPanel pnlNorth, pnlCenter, pnlSouth;
@@ -12,7 +17,7 @@ public class FrmLogin extends FrmBasic {
 
   // 생성자와 초기화 블럭은 상속되지 않는다. 멤버와 메서드만 상속
   public FrmLogin() throws HeadlessException {
-    super("Login", 270, 200);
+    super("Login", 270, 150);
   }
 
   @Override
@@ -26,13 +31,30 @@ public class FrmLogin extends FrmBasic {
     lbPass.setPreferredSize(new Dimension(50, 20));
     tfId = new JTextField(10);
     pfPass = new JPasswordField(10);
-    tfId.setFont(new Font("consolas", Font.PLAIN, 16));
-    pfPass.setFont(new Font("consolas", Font.PLAIN, 16));
+    tfId.setFont(new Font("Consolas", Font.PLAIN, 16));
+    pfPass.setFont(new Font("Consolas", Font.PLAIN, 16));
     btnLogin = new JButton("Login");
     btnJoin = new JButton("Join");
-    
     btnLogin.addActionListener(e -> {
-
+      String id = tfId.getText();
+      String pass = new String(pfPass.getPassword());
+      if (id.equals("")) {
+        JOptionPane.showMessageDialog(null, "ID를 확인해주세요.");
+        tfId.requestFocus();
+        return;
+      }
+      if (pass.equals("")) {
+        JOptionPane.showMessageDialog(null, "비밀번호를 확인해주세요.");
+        pfPass.requestFocus();
+        return;
+      }
+      MemberVO memberVO = new DAOMember().loginCheck(id, pass);
+      if (memberVO == null) {
+        JOptionPane.showMessageDialog(null, "없는 계정입니다.");
+      } else {
+        dispose();
+        
+      }
     });
     btnJoin.addActionListener(e -> {
       new FrmJoin();
@@ -46,7 +68,6 @@ public class FrmLogin extends FrmBasic {
     pnlNorth.add(tfId);
     pnlCenter.add(lbPass);
     pnlCenter.add(pfPass);
-    pnlCenter.add(btnJoin);
     pnlSouth.add(btnLogin);
     pnlSouth.add(btnJoin);
     add(pnlNorth, "North");
---
+++ src/p13database/ui/FrmMain.java
@@ -0,0 +1,26 @@
+package p13database.ui;
+
+import java.awt.*;
+
+public class FrmMain extends FrmBasic {
+
+
+  public FrmMain() throws HeadlessException {
+    super("회원목록", 500, 400);
+  }
+
+  @Override
+  public void init() {
+
+  }
+
+  @Override
+  public void arrange() {
+
+  }
+
+  @Override
+  public void inflate() {
+    super.inflate();
+  }
+}
