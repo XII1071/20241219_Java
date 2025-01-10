517ae1b02c9ac461dc64919cb6bcaf64f2c29a22
XII1071 <garmen1071@gmail.com>
2025-01-10 12:51:53 +0900

    0110

---
+++ src/p13database/dao/DAOSet.java
@@ -0,0 +1,10 @@
+package p13database.dao;
+
+import java.sql.*;
+
+public class DAOSet {
+  protected Connection.conn;
+  protected PreparedStatement.pstmt;
+  protected Statement.stmt;
+  protected ResultSet rs;
+}
--- src/p13database/ui/FrmLogin.java
+++ src/p13database/ui/FrmLogin.java
@@ -1,8 +1,15 @@
 package p13database.ui;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class FrmLogin extends FrmBasic {
+  private JPanel pnlNorth, pnlCenter, pnlSouth;
+  private JLabel lbId, lbPass;
+  private JTextField tfId;
+  private JPasswordField pfPass;
+  private JButton btnLogin, btnJoin;
+
   // 생성자와 초기화 블럭은 상속되지 않는다. 멤버와 메서드만 상속
   public FrmLogin() throws HeadlessException {
     super("Login", 270, 200);
@@ -10,12 +17,40 @@ public class FrmLogin extends FrmBasic {
 
   @Override
   public void init() {
+    pnlNorth = new JPanel();
+    pnlCenter = new JPanel();
+    pnlSouth = new JPanel();
+    lbId = new JLabel("ID");
+    lbPass = new JLabel("Pass");
+    lbId.setPreferredSize(new Dimension(50, 20));
+    lbPass.setPreferredSize(new Dimension(50, 20));
+    tfId = new JTextField(10);
+    pfPass = new JPasswordField(10);
+    tfId.setFont(new Font("consolas", Font.PLAIN, 16));
+    pfPass.setFont(new Font("consolas", Font.PLAIN, 16));
+    btnLogin = new JButton("Login");
+    btnJoin = new JButton("Join");
+    btnJoin.addActionListener(e -> {
 
+    });
+    btnLogin.addActionListener(e -> {
+      new FrmJoin();
+      dispose();
+    });
   }
 
   @Override
   public void arrange() {
-
+    pnlNorth.add(lbId);
+    pnlNorth.add(tfId);
+    pnlCenter.add(lbPass);
+    pnlCenter.add(pfPass);
+    pnlCenter.add(btnJoin);
+    pnlSouth.add(btnLogin);
+    pnlSouth.add(btnJoin);
+    add(pnlNorth, "North");
+    add(pnlCenter, "Center");
+    add(pnlSouth, "South");
   }
 
   @Override
@@ -23,3 +58,5 @@ public class FrmLogin extends FrmBasic {
     super.inflate();
   }
 }
+
+/* border layout = JFrame JDialog */
