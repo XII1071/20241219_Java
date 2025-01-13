e3a602f834ac32e677e49e392a769f9a8b34d13e
XII1071 <garmen1071@gmail.com>
2025-01-13 16:50:32 +0900

    0113

--- src/p13database/ui/FrmJoin.java
+++ src/p13database/ui/FrmJoin.java
@@ -1,20 +1,64 @@
 package p13database.ui;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class FrmJoin extends FrmBasic {
+
+  private JPanel pnlNorth, pnlCenter, pnlSouth, pnlCNorth, pnlCCenter, pnlCSouth;
+  private JTextField tfId, tfName, tfMobile;
+  private JPasswordField pfPass;
+  private JButton btnJoin, btnCancel;
+  private JLabel[] labels;
+  private String[] lbArr;
+
   public FrmJoin() throws HeadlessException {
-    super("회원가입", 270, 200);
+    super("회원가입", 270, 210);
   }
 
   @Override
   public void init() {
-
+    lbArr = new String[]{"ID", "Password", "Name", "Mobile"};
+    labels = new JLabel[4];
+    pnlNorth = new JPanel();
+    pnlCenter = new JPanel();
+    pnlSouth = new JPanel();
+    pnlCNorth = new JPanel();
+    pnlCCenter = new JPanel();
+    pnlCSouth = new JPanel();
+    tfId = new JTextField(10);
+    pfPass = new JPasswordField(10);
+    tfName = new JTextField(10);
+    tfMobile = new JTextField(10);
+    btnJoin = new JButton("Join");
+    btnCancel = new JButton("Cancel");
+    for (int i = 0; i < lbArr.length; i++) {
+      labels[i] = new JLabel(lbArr[i]);
+      labels[i].setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+      labels[i].setPreferredSize(new Dimension(80, 20));
+    }
   }
 
   @Override
   public void arrange() {
+    pnlNorth.add(labels[0]);
+    pnlNorth.add(tfId);
+    pnlCNorth.add(labels[1]);
+    pnlCNorth.add(pfPass);
+    pnlCCenter.add(labels[2]);
+    pnlCCenter.add(tfName);
+    pnlCSouth.add(labels[3]);
+    pnlCSouth.add(tfMobile);
+    pnlSouth.add(btnJoin);
+    pnlSouth.add(btnCancel);
 
+    pnlCenter.add(pnlCNorth, "North");
+    pnlCenter.add(pnlCCenter, "Center");
+    pnlCenter.add(pnlCSouth, "South");
+
+    add(pnlNorth, "North");
+    add(pnlCenter, "Center");
+    add(pnlSouth, "South");
   }
 
   @Override
